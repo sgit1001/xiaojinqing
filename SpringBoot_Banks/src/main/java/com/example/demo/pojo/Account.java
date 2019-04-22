635b39d2fc0ea7eab08486929393d9d7714459bf
Shinelon <Shinelon@192.168.9.196>
2019-04-22 17:26:02 +0800

    添加注解

--- SpringBoot_Banks/src/main/java/com/example/demo/pojo/Account.java
+++ SpringBoot_Banks/src/main/java/com/example/demo/pojo/Account.java
@@ -1,12 +1,24 @@
 package com.example.demo.pojo;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="account")
 public class Account {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "cardno")
     private String cardno;
-
+	@Column(name = "password")
     private String password;
-
+	@Column(name = "password")
     private Double balance;
-
+	@Column(name = "status")
     private Integer status;
 
     public String getCardno() {

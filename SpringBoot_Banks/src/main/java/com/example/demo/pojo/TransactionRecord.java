8754dae7f2a9ff9eb49144b78fe59ceb4008499b
Administrator <Administrator@YLMF-1801032058>
2019-04-22 16:12:13 +0800

    项目框架

---
+++ SpringBoot_Banks/src/main/java/com/example/demo/pojo/TransactionRecord.java
@@ -0,0 +1,83 @@
+package com.example.demo.pojo;
+
+public class TransactionRecord {
+    private Integer id;
+
+    private String cardno;
+
+    private String transactionDate;
+
+    private Double expense;
+
+    private Double income;
+
+    private Double balance;
+
+    private String transactionType;
+
+    private String remark;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCardno() {
+        return cardno;
+    }
+
+    public void setCardno(String cardno) {
+        this.cardno = cardno == null ? null : cardno.trim();
+    }
+
+    public String getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(String transactionDate) {
+        this.transactionDate = transactionDate == null ? null : transactionDate.trim();
+    }
+
+    public Double getExpense() {
+        return expense;
+    }
+
+    public void setExpense(Double expense) {
+        this.expense = expense;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public void setIncome(Double income) {
+        this.income = income;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType == null ? null : transactionType.trim();
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark == null ? null : remark.trim();
+    }
+}
